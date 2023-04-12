e449687fd03c6f00e05a700e360871592301422e
Nachtalb <na@nachtalb.io>
2023-04-12 17:19:28 +0200

    Make it a poetry project

--- leeplate/__init__.py
+++ leeplate/__init__.py
@@ -1,4 +1,6 @@
+import argparse
 import io
+from pathlib import Path
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse, StreamingResponse
@@ -7,10 +9,13 @@ from fastapi.templating import Jinja2Templates
 from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from gtts.lang import tts_langs
+from uvicorn import run as urun
+
+HERE = Path(__file__).parent
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=HERE / "static"), name="static")
+templates = Jinja2Templates(directory=HERE / "templates")
 translator = Translator()
 
 
@@ -57,3 +62,16 @@ async def speak(text: str, lang: str):
     tts.write_to_fp(mp3_file)
     mp3_file.seek(0)
     return StreamingResponse(mp3_file, media_type="audio/mp3")
+
+
+def run():
+    parser = argparse.ArgumentParser(
+        description="Leeplate: A privacy-oriented alternative frontend for translation providers."
+    )
+    parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to run the server on (default: 127.0.0.1)")
+    parser.add_argument("--port", type=int, default=8000, help="Port to run the server on (default: 8000)")
+    parser.add_argument("--reload", action="store_true", help="Enable auto-reloading of the server")
+
+    args = parser.parse_args()
+
+    urun(app, host=args.host, port=args.port, reload=args.reload)
